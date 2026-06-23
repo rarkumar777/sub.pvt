b257c0045e3535fe891a560fde0cad5920c496d8
aayukumar121361-glitch <aayukumar121361@gmail.com>
2026-06-23 22:08:09 +0530

    Add vendor for production

---
+++ vendor/laravel/framework/src/Illuminate/Http/Middleware/ValidatePostSize.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace Illuminate\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Exceptions\PostTooLargeException;
+
+class ValidatePostSize
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     *
+     * @throws \Illuminate\Http\Exceptions\PostTooLargeException
+     */
+    public function handle($request, Closure $next)
+    {
+        // Patched: skip post size validation to allow large file/video uploads
+        return $next($request);
+    }
+
+    /**
+     * Determine the server 'post_max_size' as bytes.
+     *
+     * @return int
+     */
+    protected function getPostMaxSize()
+    {
+        if (is_numeric($postMaxSize = ini_get('post_max_size'))) {
+            return (int) $postMaxSize;
+        }
+
+        $metric = strtoupper(substr($postMaxSize, -1));
+
+        $postMaxSize = (int) $postMaxSize;
+
+        return match ($metric) {
+            'K' => $postMaxSize * 1024,
+            'M' => $postMaxSize * 1048576,
+            'G' => $postMaxSize * 1073741824,
+            default => $postMaxSize,
+        };
+    }
+}
